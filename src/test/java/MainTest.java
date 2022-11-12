9efbf39c7f6d766ea2f14192d8525864bd1d086f
dmytroprima <112015667+dmytroprima@users.noreply.github.com>
2022-11-13 01:20:59 +0200

    Add files via upload

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -10,11 +10,10 @@ import java.io.ByteArrayInputStream;
 
 import static com.menu.Main.findGift;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class MainTest {
-    @Mock
-    public Gift gift = new Gift("s");
     @Test
     public void createGiftTest(){
         String simulatedUserInput = "gift" +  "\n"
@@ -25,12 +24,11 @@ public class MainTest {
     }
     @Test
     public void SortGiftTest(){
-        //Gift gift = new Gift("gift");
+        Gift gift = new Gift("gift");
         gift.addCandy(new Candy("b",1,1,1));
         gift.addCandy(new Candy("a", 2,2,2));
         String simulatedUserInput = "0" + "\n" + "ABC" +  "\n";
         System.setIn(new ByteArrayInputStream(simulatedUserInput.getBytes()));
-        //when(findGift()).thenReturn(gift);
         given(findGift()).willReturn(gift);
         Assert.assertEquals("a", Main.sortGift().getCandies().get(0).getName());
     }
